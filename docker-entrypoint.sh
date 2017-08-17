248a9ca101d67fa165407ced2ea9161a5e78247d
David Morcillo <david.morcillo@gmail.com>
2017-08-17 12:26:22 +0200

    Fix a few things related to the version name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -54,15 +54,15 @@ bundle exec rake decidim:upgrade db:migrate
 echo "6. Create/Update PR to update decidim"
 branch_name="update-decidim-$decidim_version"
 git checkout -b $branch_name
-git add . && git commit -am "Update decidim to $decidim_version" && git push origin $branch_name
+git add . && git commit -am "Update decidim to v$decidim_version" && git push origin $branch_name
 curl \
   -i \
   -X POST \
   -d "{
-    \"title\": \"Update Decidim to $decidim_version\",
+    \"title\": \"Update Decidim to v$decidim_version\",
     \"base\":\"master\",
     \"head\":\"$GITHUB_ORGANIZATION:$branch_name\",
-    \"body\":\"Update decidim from $old_decidim_version to $decidim_version. You can check the release notes [here](https://github.com/decidim/decidim/releases/tag/$decidim_version).\",
+    \"body\":\"Update decidim from v$old_decidim_version to v$decidim_version. You can check the release notes [here](https://github.com/decidim/decidim/releases/tag/v$decidim_version).\",
     \"maintainer_can_modify\":true
   }" \
   https://api.github.com/repos/$DECIDIM_GITHUB_ORGANIZATION/$DECIDIM_GITHUB_REPO/pulls?access_token=$GITHUB_OAUTH_TOKEN
