d9d58225baf98d14c260e0c6e1843b58f2d35e61
David Morcillo <david.morcillo@gmail.com>
2017-08-17 12:19:55 +0200

    Update to the latest workflow

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 now=`date +"%s"`
 
-function get_revision() {
+function get_decidim_version() {
   bundle show decidim | awk -F'-' '{print $NF}'
 }
 
@@ -34,22 +34,8 @@ function get_last_timestamp() {
 git config --global user.name $GIT_USERNAME
 git config --global user.email $GIT_EMAIL
 
-echo "0. Get last pr info"
-curl https://api.github.com/repos/$DECIDIM_GITHUB_ORGANIZATION/$DECIDIM_GITHUB_REPO/pulls?access_token=$GITHUB_OAUTH_TOKEN > last_prs.json
-last_id=$(get_last_id $last_pr_info)
-last_timestamp=$(get_last_timestamp $last_pr_info)
-
 echo "1. Clone repository"
 git clone https://$GITHUB_OAUTH_TOKEN@github.com/$GITHUB_ORGANIZATION/$GITHUB_REPO.git && cd $GITHUB_REPO
-git remote add decidim https://github.com/$DECIDIM_GITHUB_ORGANIZATION/$DECIDIM_GITHUB_REPO.git && git fetch decidim
-git reset --hard decidim/master
-if [ "$last_timestamp" == "" ]; then
-  branch_name="update-decidim-${DECIDIM_VERSION:-$now}"
-  git checkout -b $branch_name
-else
-  branch_name="update-decidim-$last_timestamp"
-  git checkout $branch_name
-fi
 
 echo "2. Install gems"
 bundle install
@@ -58,38 +44,25 @@ echo "3. Create db and load schema"
 bundle exec rake db:create db:schema:load
 
 echo "4. Update decidim"
-old_revision=$(get_revision)
+old_decidim_version = get_decidim_version()
 bundle update decidim decidim-dev
-new_revision=$(get_revision)
-if [ "$old_revision" == "$new_revision" ]; then
-  echo "Nothing to do."
-  exit 0
-fi
+decidim_version = get_decidim_version()
 
 echo "5. Run decidim:upgrade and migrate db"
 bundle exec rake decidim:upgrade db:migrate
 
 echo "6. Create/Update PR to update decidim"
-git add . && git commit -am "Update decidim" && git push origin $branch_name
-
-if [ "$last_timestamp" == "" ]; then
-  curl \
-    -i \
-    -X POST \
-    -d "{
-      \"title\": \"Update Decidim\",
-      \"base\":\"master\",
-      \"head\":\"$GITHUB_ORGANIZATION:$branch_name\",
-      \"body\":\"Changes https://github.com/decidim/decidim/compare/$old_revision...$new_revision\",
-      \"maintainer_can_modify\":true
-    }" \
-    https://api.github.com/repos/$DECIDIM_GITHUB_ORGANIZATION/$DECIDIM_GITHUB_REPO/pulls?access_token=$GITHUB_OAUTH_TOKEN
-else
-  curl \
-    -i \
-    -X POST \
-    -d "{
-      \"body\":\"Changes https://github.com/decidim/decidim/compare/$old_revision...$new_revision\"
-    }" \
-    https://api.github.com/repos/$DECIDIM_GITHUB_ORGANIZATION/$DECIDIM_GITHUB_REPO/issues/$last_id/comments?access_token=$GITHUB_OAUTH_TOKEN
-fi
+branch_name="update-decidim-$decidim_version"
+git checkout -b $branch_name
+git add . && git commit -am "Update decidim to $decidim_version" && git push origin $branch_name
+curl \
+  -i \
+  -X POST \
+  -d "{
+    \"title\": \"Update Decidim to $decidim_version\",
+    \"base\":\"master\",
+    \"head\":\"$GITHUB_ORGANIZATION:$branch_name\",
+    \"body\":\"Update decidim from $old_decidim_version to $decidim_version. You can check the release notes [here](https://github.com/decidim/decidim/releases/tag/$decidim_version).\",
+    \"maintainer_can_modify\":true
+  }" \
+  https://api.github.com/repos/$DECIDIM_GITHUB_ORGANIZATION/$DECIDIM_GITHUB_REPO/pulls?access_token=$GITHUB_OAUTH_TOKEN
